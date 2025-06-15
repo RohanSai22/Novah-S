126e0323949b29497fec06c073252d555ac35bfa
RohanSai22 <96601297+RohanSai22@users.noreply.github.com>
2025-06-15 22:05:37 +0530

    Added analytics

--- .history/src/App_20250614164652.tsx
+++ .history/src/App_20250614164652.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ChatPage from "./pages/ChatPage";
 import NotFound from "./pages/NotFound";
-import { Analytics } from "@vercel/analytics/next";
 
 const queryClient = new QueryClient();
 
